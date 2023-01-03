2e111178506ba1c600fd773da1f62de0b1551fba
FixFire <89625074+64070148NatthawutSornkhiew@users.noreply.github.com>
2023-01-03 14:05:01 +0700

    Update EachStudent.vue

--- src/components/EachStudent.vue
+++ src/components/EachStudent.vue
@@ -1,29 +1,32 @@
-
 <template>
+  <CardStudent :gender="gender">
+    <template v-slot:card-header>
+      <p>
+        {{ gender === "ชาย" ? "นาย" : "นาง" }} {{ name === "" ? "test" : name }}
+      </p>
+      <p>รหัส {{ !isNaN(parseInt(id)) ? id : "5555" }}</p>
+    </template>
 
-    <CardStudent :gender="gender">
-      <template v-slot:card-header>
-        <p> {{ gender === "ชาย" ? "นาย" : "นาง" }}  {{ name }}</p>
-        <p>รหัสนักศึกษา {{ id }}</p>
-      </template>
-
-      <template v-slot:card-content>
+    <template v-slot:card-content>
+      <div v-show="false">
         <p>{{ branch }}</p>
         <p>{{ saka }}</p>
-      </template>
-      <template v-slot:card-button>
-        <button @click="delData(id)">ลบข้อมูล</button>
-      </template>
+      </div>
+    </template>
 
-    </CardStudent>
+    <template v-slot:card-button>
+      <button>รายละเอียด</button><br />
+      <button @click="delData(id)">ลบข้อมูล</button>
+    </template>
+  </CardStudent>
 </template>
 
 <script>
-import CardStudent from './CardStudent.vue'
+import CardStudent from "./CardStudent.vue";
 export default {
-  name : "EachStudent",
-  components:{
-    CardStudent
+  name: "EachStudent",
+  components: {
+    CardStudent,
   },
   props: {
     name: {
@@ -35,37 +38,34 @@ export default {
     branch: {
       type: String,
     },
-    saka:{
-      type:String
-    },
-    gender:{
+    saka: {
       type: String,
-    }
-    
+    },
+    gender: {
+      type: String,
+    },
   },
-  methods:{
-    delData(id){
-      this.$emit("del", id)
+  methods: {
+    delData(id) {
+      this.$emit("del", id);
       // console.log(id)
-
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
-  p{
-    font-size: 1rem;
-  }
-  button{
-    font:inherit;
-    background: rgb(237, 195, 180);
-    color:black;
-    cursor: pointer;
-    margin-top: 1rem ;
-    padding: 0.25rem 2rem;
-    border-radius: 15px;
-    font-size: 1rem;
-  }
-
+p {
+  font-size: 1rem;
+}
+button {
+  font: inherit;
+  background: rgb(237, 195, 180);
+  color: black;
+  cursor: pointer;
+  margin-top: 1rem;
+  padding: 0.25rem 2rem;
+  border-radius: 15px;
+  font-size: 1rem;
+}
 </style>
